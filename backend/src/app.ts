4469488750dfc64e6c798a1ba64ac43ff0bcb3e8
Sujeeth-Varma <sujeethvarma27@gmail.com>
2025-11-14 12:53:53 +0530

    init backend

---
+++ backend/src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Fitness Planner Backend Running");
+});
+
+export default app;
